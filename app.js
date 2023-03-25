53d6e57162cffbdcbc11321370d5f6898fcb329e
Ranonx <108208227+Ranonx@users.noreply.github.com>
2023-03-25 12:03:39 +0800

    single pdf upload done

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const multer = require("multer");
-const path = require("path");
 const fs = require("fs");
-const { processPDF } = require("./pdfProcessor");
+const { processPDF, extractFootData } = require("./pdfProcessor");
+const mysql = require("mysql");
 
 const app = express();
 const upload = multer({ dest: "uploads/" });
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -16,8 +18,48 @@ app.get("/", (req, res) => {
 app.post("/upload", upload.single("pdf"), async (req, res) => {
   const pdfPath = req.file.path;
   const data = await processPDF(pdfPath);
+  const footData = extractFootData(data);
   fs.unlinkSync(pdfPath); // Delete the uploaded file
-  res.render("table", { data: data });
+  res.render("table", { footData }, (err, html) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error rendering table");
+    } else {
+      res.send(html);
+    }
+  });
+});
+
+app.post("/insert", async (req, res) => {
+  const footData = req.body;
+  console.log(`recieved footData: ${footData}`);
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Rc19931020",
+    database: "unisole",
+  });
+
+  connection.connect();
+
+  const query = `
+    INSERT INTO foot_data (id_num, name, gender, shoe_size_left, shoe_size_right,
+      arch_length_left, arch_length_right, arch_width_left, arch_width_right, heel_width_left, heel_width_right,
+      foot_length_left, foot_length_right, foot_width_left, foot_width_right, ball_girth_left, ball_girth_right,
+      arch_index_left, arch_index_right, arch_ratio_left, arch_ratio_right)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  connection.query(query, Object.values(footData), (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Error inserting data");
+    } else {
+      res.status(200).send("Data inserted successfully");
+    }
+  });
+
+  connection.end();
 });
 
 app.listen(3000, () => {
--- pdfProcessor.js
+++ pdfProcessor.js
@@ -1,8 +1,34 @@
 const fs = require("fs");
 const path = require("path");
-const { PDFDocument } = require("pdf-lib");
 const pdfjs = require("pdfjs-dist/build/pdf");
-const pdfjsWorker = require("pdfjs-dist/build/pdf.worker.entry");
+
+function extractFootData(data) {
+  const footData = {
+    id_num: data[10],
+    name: data[11],
+    gender: data[12],
+    shoe_size_left: parseInt(data[18], 10),
+    shoe_size_right: parseInt(data[19], 10),
+    arch_length_left: parseFloat(data[53]),
+    arch_length_right: parseFloat(data[54]),
+    arch_width_left: parseFloat(data[55]),
+    arch_width_right: parseFloat(data[56]),
+    heel_width_left: parseFloat(data[57]),
+    heel_width_right: parseFloat(data[58]),
+    foot_length_left: parseFloat(data[59]),
+    foot_length_right: parseFloat(data[60]),
+    foot_width_left: parseFloat(data[61]),
+    foot_width_right: parseFloat(data[62]),
+    ball_girth_left: parseInt(data[63], 10),
+    ball_girth_right: parseInt(data[64], 10),
+    arch_index_left: parseFloat(data[65]),
+    arch_index_right: parseFloat(data[66]),
+    arch_ratio_left: parseFloat(data[67]),
+    arch_ratio_right: parseFloat(data[68]),
+  };
+
+  return footData;
+}
 
 async function processPDF(pdfPath) {
   // Load the PDF file
@@ -11,26 +37,24 @@ async function processPDF(pdfPath) {
   // Use pdfjs to extract text from the PDF
   const loadingTask = pdfjs.getDocument({ data: pdfBytes });
   const pdf = await loadingTask.promise;
-  const numPages = pdf.numPages;
 
-  const pageTextPromises = [];
-  for (let pageNum = 1; pageNum <= numPages; pageNum++) {
-    pageTextPromises.push(
-      pdf.getPage(pageNum).then(async (page) => {
-        const textContent = await page.getTextContent();
-        return textContent.items.map((item) => item.str).join(" ");
-      })
-    );
-  }
-
-  const pageTexts = await Promise.all(pageTextPromises);
+  // Get the first page
+  const firstPage = await pdf.getPage(1);
+  const textContent = await firstPage.getTextContent();
+  const pageText = textContent.items.map((item) => item.str).join(" ");
 
   // Extract data from the PDF
-  const data = pageTexts; // Array of text from each page
+  const dataArray = pageText.split(/\s+/); // Split the text by whitespace and line breaks
 
-  return data;
+  // Log the extracted data
+  dataArray.forEach((item, index) => {
+    // console.log(`${index}: ${item}`);
+  });
+
+  return dataArray;
 }
 
 module.exports = {
   processPDF,
+  extractFootData,
 };
